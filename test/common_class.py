703fd9c844dc25a3363b186939970a4ffaf23d55
Mahesh Rayas <maheshrayas@gmail.com>
2019-04-11 22:21:22 +1000

    Added few more test cases

--- test/common_class.py
+++ test/common_class.py
@@ -1,5 +1,5 @@
 from deploy_stack.deploy_stack import StackDeployer
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 import sys
 import logging
 from moto import mock_s3
@@ -36,3 +36,69 @@ class CommonClass():
         with patch.object(sys, 'argv', testargs):
             deploy_stack_object = StackDeployer()
         return deploy_stack_object
+
+
+class Options:
+    def __init__(self):
+        self.installation_name = 'rco'
+        self.component_name = 'generic-ops'
+        self.dns_domain = 'test.innablr.cld'
+        self.org_arn = None
+        self.runtime_environment = 'prod'
+        self.parameters_dir = 'test/parameters'
+
+
+class CloudformationEnvironment():
+    @patch('deploy_stack.deploy_stack.s')
+    def __init__(self, mock_session):
+        self.s3_bucket = CommonClass(mock_session)._bucket
+        self.installation_name = Options().installation_name
+        self.dns_domain = Options().dns_domain
+        self.runtime_environment = Options().runtime_environment
+        self.templates = CloudformationCollection()
+        self.parameters_dir = Options().parameters_dir
+
+
+class CloudformationCollection():
+    @patch('deploy_stack.deploy_stack.s')
+    def __init__(self, mock_session):
+        self.s3_bucket = CommonClass(mock_session)._bucket
+        self.environment_parameters = {'ssm-parameters': {'RootServicesAccountId': '24234234324234',
+        'SharedServicesAccountId': '13123123123',
+         'LoggingAccountId': '1232144325235'},
+         'AllConnectedAccounts':
+         'arn:aws:iam::000000000000:root, arn:aws:iam::111111111111:root,arn:aws:iam::222222222222:root',
+         'stacks': [{'name': 'my-project-kms-decrypt-lambda', 'template': 'support/kms-parameters-lambda.cf.yaml',
+         'parameters': {'LambdaSourceS3Key': 'kmsParameters.zip'}}]}
+        self.template_files = [('support/kms-parameters-lambda.cf.yaml',
+        'test/cloudformation/support/kms-parameters-lambda.cf.yaml')]
+        self.templates = CloudformationTemplate()
+
+    def find_template(self, template_name: str):
+        d = {'template_url': 'kms-parameters-lambda.cf.yaml', 'template_s3_key ': 'myS3Key'}
+        return Mock(**d)
+
+
+class CloudformationTemplate():
+    def __init__(self):
+        self.template_key: str = 'support/kms-parameters-lambda.cf.yaml'
+        self.template_parameters = {'name': 'my-project-kms-decrypt-lambda',
+        'template': 'support/kms-parameters-lambda.cf.yaml',
+        'parameters': {'LambdaSourceS3Key': 'kmsParameters.zip'}}
+        self.template_body = CloudformationTemplateBody()
+        self.s3_key_prefix: str = 'cloudformation'
+        self.s3_key: str = 'support/7d413e7a42238ddb5eb35ef3c4d08c5c717a5e5b-kms-parameters-lambda.cf.yaml'
+        self.u = 'False'
+
+
+class CloudformationTemplateBody():
+    def __init__(self) -> None:
+        self.text = 'Testing'
+        self.checksum = '213123123123'
+        self.body = {'Key': 'Value'}
+
+
+class Template():
+    def __init__(self):
+        self.name = 'logging-set'
+        self.template_type = 'stackset'
